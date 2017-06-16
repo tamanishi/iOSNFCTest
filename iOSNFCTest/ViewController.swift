881ac46fb3368d100e07cf2a3609903cde5550d5
tamanishi <tamanishi@gmail.com>
2017-06-17 08:57:14 +0900

    Initial Commit

---
+++ iOSNFCTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSNFCTest
+//
+//  Created by msyksnhr on 2017/06/17.
+//  Copyright © 2017 tamanishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
