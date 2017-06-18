ae8b50daff6f792c0bc295210a325a08340665b9
tamanishi <tamanishi@gmail.com>
2017-06-18 12:38:06 +0900

    http://qiita.com/bellx2/items/3498aa7637f96a966854 を見て試してみました。

--- iOSNFCTest/ViewController.swift
+++ iOSNFCTest/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    
+    @IBOutlet weak var messageField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func startScanning(_ sender: Any) {
+        let session:NFCNDEFReaderSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
+        session.begin()
+        print("Session started")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for payload in message.records {
+                print("Payload data: \(payload.payload)")
+            }
+        }
+    }
+    
 }
 
