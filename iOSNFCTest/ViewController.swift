30a49300f8ba145c1a7df10e1db0c978dd0a3795
tamanishi <tamanishi@gmail.com>
2017-06-23 07:40:12 +0900

    読み取ったデータをlabelに表示するようにした

--- iOSNFCTest/ViewController.swift
+++ iOSNFCTest/ViewController.swift
@@ -11,11 +11,11 @@ import CoreNFC
 
 class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
     
-    @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var payloadLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        payloadLabel.text = nil
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,18 +27,25 @@ class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
         let session:NFCNDEFReaderSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: false)
         session.begin()
         print("Session started")
+        payloadLabel.text = nil
     }
     
     func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
         print("Error: \(error.localizedDescription)")
+        let alertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
     }
     
     func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        var payloadString: String = String()
         for message in messages {
             for payload in message.records {
-                print("Payload data: \(payload.payload)")
+                payloadString += String(describing: payload.payload)
             }
         }
+        payloadLabel.text = payloadString
     }
     
 }
